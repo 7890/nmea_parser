4e7d9a8802cdf1f052cae476618547c1ecc74bd3
Thomas Brand <tom@trellis.ch>
2016-03-06 13:50:14 +0100

    create from object of superclass type

--- src/OSCGPSPosition.java
+++ src/OSCGPSPosition.java
@@ -7,6 +7,30 @@ public class OSCGPSPosition extends ConvertibleGPSPosition
 /*
 /pos hhsfffifffffffsiifff 1456227925005 1457265094441 "20160223" 134525.500000 7.636898 46.907036 2 -1.000000 754.400024 0.054428 0.280000 0.230000 0.780000 0.400000 "D" 7 3 2.360000 1.330000 1.950000
 */
+//========================================================================
+	public OSCGPSPosition(GPSPosition pos)
+	{
+		this.millis_utc=	pos.millis_utc;
+		this.millis_utc_sys=	pos.millis_utc_sys;
+		this.date=		pos.date;
+		this.time=		pos.time;
+		this.lon=		pos.lon;
+		this.lat=		pos.lat;
+		this.quality=		pos.quality;
+		this.direction=		pos.direction;
+		this.altitude=		pos.altitude;
+		this.velocity=		pos.velocity;
+		this.lat_err=		pos.lat_err;
+		this.lon_err=		pos.lon_err;
+		this.alt_err=		pos.alt_err;
+		this.dgps_age=		pos.dgps_age;
+		this.mode=		pos.mode;
+		this.sat_in_use=	pos.sat_in_use;
+		this.fix_type=		pos.fix_type;
+		this.PDOP=		pos.PDOP;
+		this.HDOP=		pos.HDOP;
+		this.VDOP=		pos.VDOP;
+	}
 
 //========================================================================
 	public OSCMessage getValuesAsOSCMessage()
