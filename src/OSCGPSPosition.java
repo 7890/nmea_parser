39931e14e5cc6357d244f598c17fd11db152ffd6
Thomas Brand <tom@trellis.ch>
2016-03-08 02:02:32 +0100

    add constructors previously removed by accident

--- src/OSCGPSPosition.java
+++ src/OSCGPSPosition.java
@@ -4,6 +4,33 @@ import com.illposed.osc.OSCMessage;
 //=============================================================================
 public class OSCGPSPosition extends ConvertibleGPSPosition
 {
+	public OSCGPSPosition(){}
+
+//========================================================================
+	public OSCGPSPosition(GPSPosition pos)
+	{
+		this.millis_utc=	pos.millis_utc;
+		this.millis_utc_sys=	pos.millis_utc_sys;
+		this.date=		pos.date;
+		this.time=		pos.time;
+		this.lon=		pos.lon;
+		this.lat=		pos.lat;
+		this.quality=		pos.quality;
+		this.direction=		pos.direction;
+		this.altitude=		pos.altitude;
+		this.velocity=		pos.velocity;
+		this.lat_err=		pos.lat_err;
+		this.lon_err=		pos.lon_err;
+		this.alt_err=		pos.alt_err;
+		this.dgps_age=		pos.dgps_age;
+		this.mode=		pos.mode;
+		this.sat_in_use=	pos.sat_in_use;
+		this.fix_type=		pos.fix_type;
+		this.PDOP=		pos.PDOP;
+		this.HDOP=		pos.HDOP;
+		this.VDOP=		pos.VDOP;
+	}
+
 //========================================================================
 	public OSCMessage getValuesAsOSCMessage()
 	{
